f995bf907da3cf93c5879e44edbd0147e9432307
jlclx <46258217+jlclx@users.noreply.github.com>
2025-02-15 14:10:00 -0600

    Add wsl2 clipboard support

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,15 @@ require "nvchad.options"
 
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
+
+
+-- WSL2 clipboard support
+vim.o.clipboard = "unnamedplus"
+if vim.fn.has('wsl') == 1 then
+  vim.api.nvim_create_autocmd({'TextYankPost'}, {
+    group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+    callback = function()
+      vim.fn.system('clip.exe', vim.fn.getreg('"'))
+    end,
+  })
+end
